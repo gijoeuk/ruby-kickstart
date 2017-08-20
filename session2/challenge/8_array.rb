f6fb485675045d980033f032b4937bc9a86f41a7
gijoeuk <josephcingram@hotmail.co.uk>
2017-08-20 18:40:38 +0100

    Add solution 2.8

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,6 +5,6 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
-def thrice?(array)
-   array.each_cons(3) {|a, b, c| return true if a == b && b == c}
+def got_three?(array)
+   array.each_cons(3){|a, b, c| return true if a == b && b == c}
 end
